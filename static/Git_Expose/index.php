646641f872b93f4a85868a7f8e27f03e63b2f317
anonymous3 <anonymous@notgonnafind.me>
2020-09-17 18:57:16 +0530

    new chal

---
+++ static/Git_Expose/index.php
@@ -0,0 +1,17 @@
+<?php
+
+
+	include '../common/template.php';
+
+	error_reporting(0);
+
+	$flag = "flag{youare_too_good_for_sjcctf}";
+	
+	show_html_head("Git Raid");
+
+	echo "what happens when you expose a .git folder";
+
+	show_html_foot();
+
+?>
+
